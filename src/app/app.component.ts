48b413d255a36ad89a0261726b3f25aa3be3dbd9
Callum Eden <callum.eden15@imperial.ac.uk>
2019-04-26 17:47:33 +0100

    Getting basic visualisation working for some mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import APP_CONFIG from './app.config';
+import { Node, Link, BlockNode} from './d3';
+import {BitcoinService} from './bitcoin/bitcoin.service'
+import { Block } from './bitcoin/model/block'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'forensic-ui';
+
+export class AppComponent implements OnInit {
+
+  nodes: Node[] = [];
+  links: Link[] = [];
+  blockData: Block;
+
+  constructor(private bitcoinService : BitcoinService) {
+    const N = APP_CONFIG.N,
+          getIndex = number => number - 1;
+
+    /** constructing the nodes array */
+    for (let i = 1; i <= N; i++) {
+      this.nodes.push(new Node(i));
+    }
+
+    for (let i = 1; i <= N; i++) {
+      for (let m = 2; i * m <= N; m++) {
+        /** increasing connections toll on connecting nodes */
+        this.nodes[getIndex(i)].linkCount++;
+        this.nodes[getIndex(i * m)].linkCount++;
+
+        /** connecting the nodes before starting the simulation */
+        this.links.push(new Link(i, i * m));
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.getBlock();
+  }
+
+  getBlock(): void {
+    this.bitcoinService
+      .getBlock("0000000067a97a2a37b8f190a17f0221e9c3f4fa824ddffdc2e205eae834c8d7")
+      .subscribe((blockData : Block) => {
+        this.blockData = blockData;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GraphComponent } from './graph/graph.component';
-
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+
+import { D3Service, D3_DIRECTIVES } from './d3';
+
+import { AppComponent } from './app.component';
+
+import { GraphComponent } from './visuals/graph/graph.component';
+import { SHARED_VISUALS } from './visuals/shared';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphComponent
+    GraphComponent,
+    ...SHARED_VISUALS,
+    ...D3_DIRECTIVES
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
